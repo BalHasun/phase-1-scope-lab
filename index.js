d90da19a317e4f38905ae6f4e1402e28379ac804
Bal Elsada Hasun <fouadhason@gmail.com>
2023-04-27 10:19:20 +0300

    Upload Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+  return customerName
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+   bestCustomer  = 'not bob'
+}
+setBestCustomer()
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+  return bestCustomer
+}
+
+const leastFavoriteCustomer = 'Bal'
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'maybe bob'
+  return leastFavoriteCustomer
+}
